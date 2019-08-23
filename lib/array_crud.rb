d20df9bbeb578cd8a00e2805381aa745497c61fd
Panela Leung <leungpanela@gmail.com>
2019-08-23 20:40:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  planets=[venus, mars, earth, jupiter]
+  []
 end
 
 def create_an_array(array,element)
-array << element
+planets=[venus, mars, earth, jupiter]
 end
 
 def add_element_to_end_of_array(array, element)
